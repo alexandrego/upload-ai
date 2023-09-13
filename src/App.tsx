651e09f533860ba56e562b6e71104bfb385d5469
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2023-09-13 17:55:23 -0300

    chore: 37m

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1>Upload AI</h1>
+
+        <div>
+          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
